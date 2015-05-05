e753c014161f4674aa9fcba13aa08131b58efb13
Tener Desenvolvimento <rubydev@Teners-Mac-mini.local>
2015-05-05 19:25:08 -0300

    Funcionando o busca

--- trie.py
+++ trie.py
@@ -11,7 +11,7 @@ class Trie:
             self.palavras.append([])
             return
         else:
-            for i in range(len(palavra) - 1):
+            for i in range(len(palavra)):
                 if palavra[:i] in self.palavras:
                     self.palavras[self.palavras.index(palavra[:i]) + 1].append(palavra[i:])
                     return
@@ -21,7 +21,6 @@ class Trie:
                     if palavra == self.palavras[x][:i]:
                         self.palavras.insert(x, palavra)
                         aux = self.palavras.pop(x + 1)[i:]
-                        print aux
                         for j in range(len(self.palavras[x + 1])):
                             aux2 = self.palavras[x + 1].pop(j)
                             self.palavras[x + 1].insert(j, aux + aux2)
@@ -31,6 +30,16 @@ class Trie:
         self.palavras.append(palavra)
         self.palavras.append([])
 
+    def busca(self, palavra):
+        for i in range(len(palavra) + 1): #percorre a palavra
+            for j in range(len(self.palavras)): #percorre a lista
+                if palavra[:i] == self.palavras[j]: #compara o pedaco da palavra com a palavra da lista
+                    aux = palavra[:i] #atribui o pedaco da palavra a uma variavel
+                    if palavra == self.palavras[j]:
+                        print palavra
+                    for x in range(len(self.palavras[j + 1])): #percorre o array referente ao pedaco da palavra
+                        if palavra[i:] in self.palavras[j + 1][x]:
+                            print aux + self.palavras[j + 1][x]
 
 
 
@@ -48,3 +57,5 @@ x.incluir("Hugo")
 print x
 x.incluir("Hu")
 print x
+x.busca("Hu")
+x.busca("Pato")
