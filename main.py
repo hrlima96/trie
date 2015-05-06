304d1dff7c29a30aec67001223ff2e57729b94ed
Felipe Farias <carlosfelipefarias@gmail.com>
2015-05-05 21:16:16 -0300

    ...

--- main.py
+++ main.py
@@ -2,16 +2,10 @@ from trie import Trie
 
 x = Trie()
 x.incluir("Pato")
-print x
 x.incluir("Patota")
-print x
 x.incluir("Patotinha")
-print x
 x.incluir("Pa")
-print x
 x.incluir("Hugo")
-print x
 x.incluir("Hu")
-print x
 x.busca("Hu")
 x.busca("Pato")
